482097938b2ccd59be760ee9ea5a4da04154ee9a
Sijmen Schoon <vijfhoek35@gmail.com>
2014-04-19 15:55:01 +0200

    Replaced tabs with spaces

--- Graphics/src/Textures/Texture.h
+++ Graphics/src/Textures/Texture.h
@@ -6,14 +6,14 @@
 class Texture
 {
 public:
-	Texture();
-	Texture(const std::string &path);
-	~Texture();
+    Texture();
+    Texture(const std::string &path);
+    ~Texture();
 
     bool IsLoaded() const { return mTexID; };
 
 private:
-	unsigned int mTexID;
+    unsigned int mTexID;
 };
 
 #endif
